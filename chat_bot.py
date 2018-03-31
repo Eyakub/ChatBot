7497edf3cf2bca198c94d669afeedd564df6558f
Eyakub <eyakubsorkar@gmail.com>
2018-03-31 13:32:16 +0600

    new commit

---
+++ chat_bot.py
@@ -0,0 +1,24 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+import os
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+bot = ChatBot('Bot')
+bot.set_trainer(ListTrainer)
+
+for files in os.listdir('C:\Python27\Lib\site-packages\chatterbot_corpus\data\Bangla/'):
+    data = open('C:\Python27\Lib\site-packages\chatterbot_corpus\data\Bangla/' + files , 'r').readlines()
+    bot.train(data)
+
+
+while True:
+    message = raw_input('You: ')
+    if message.strip() != 'Bye':
+        reply = bot.get_response(message)
+        print 'ChatBot:' , reply
+    if message.strip() == 'Bye':
+        print'ChatBot : Bye'
+        break
+
